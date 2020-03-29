501f0ba5a0cb89f02319d76035d99c0d61254131
gillessed <gtc.cole@gmail.com>
2020-03-28 17:18:21 -0700

    Change spacing.

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,41 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-    mode: 'development',
-    target: 'node',
-    entry: {
-        app: ['webpack/hot/dev-server', './app/app.tsx'],
-    },
-    output: {
-        path: path.join(__dirname, './devServer/built'),
-        filename: 'bundle.js',
-        publicPath: 'http://localhost:8080/built/'
-    },
-    devServer: {
-        contentBase: './devServer',
-        publicPath: 'http://localhost:8080/built/'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', 'jsx']
-    },
-    module: {
-        rules: [
-            { test: /\.(tsx?)$/, loader: 'ts-loader', exclude: /node_modules/ },
-            { test: /\.(jsx?)$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-        ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
+  mode: 'development',
+  target: 'node',
+  entry: {
+    app: ['webpack/hot/dev-server', './app/app.tsx'],
+  },
+  output: {
+    path: path.join(__dirname, './devServer/built'),
+    filename: 'bundle.js',
+    publicPath: 'http://localhost:8080/built/'
+  },
+  devServer: {
+    contentBase: './devServer',
+    publicPath: 'http://localhost:8080/built/'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', 'jsx']
+  },
+  module: {
+    rules: [
+      { test: /\.(tsx?)$/, loader: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.(jsx?)$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
